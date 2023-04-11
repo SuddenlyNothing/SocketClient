1bab95a58983f1d5201a059264b2d9259f76d645
Bix Men <72772195+SuddenlyNothing@users.noreply.github.com>
2023-04-10 22:15:41 -0700

    Remove comments

--- SocketClient.java
+++ SocketClient.java
@@ -1,12 +1,6 @@
 import java.net.*;
 import java.io.*;
 
-/*
-* java .\SocketClient.java djxmmx.net 17
-* java .\SocketClient.java time.nist.gov 13
-* java .\SocketClient.java tcpbin.com 4242
-*/
-
 public class SocketClient {
   public static void main(String[] args) {
     if (args.length < 2) {
@@ -40,41 +34,3 @@ public class SocketClient {
     }
   }
 }
-
-// import java.io.*;
-// import java.net.*;
-
-// public class SocketClient {
-// public static void main(String[] args) throws IOException {
-
-// if (args.length != 2) {
-// System.err.println(
-// "Usage: java EchoClient <host name> <port number>");
-// System.exit(1);
-// }
-
-// String hostName = args[0];
-// int portNumber = Integer.parseInt(args[1]);
-
-// try (
-// Socket echoSocket = new Socket(hostName, portNumber);
-// PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-// BufferedReader in = new BufferedReader(
-// new InputStreamReader(echoSocket.getInputStream()));
-// BufferedReader stdIn = new BufferedReader(
-// new InputStreamReader(System.in))) {
-// String userInput;
-// while ((userInput = stdIn.readLine()) != null) {
-// out.println(userInput);
-// System.out.println("echo: " + in.readLine());
-// }
-// } catch (UnknownHostException e) {
-// System.err.println("Don't know about host " + hostName);
-// System.exit(1);
-// } catch (IOException e) {
-// System.err.println("Couldn't get I/O for the connection to " +
-// hostName);
-// System.exit(1);
-// }
-// }
-// }
